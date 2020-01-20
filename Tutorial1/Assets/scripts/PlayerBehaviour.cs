d43600d25b238aeaf20f944843ae17a8603f9c06
DaRkSoUrC3 <ak20sv1998@gmail.com>
2020-01-20 16:36:32 +0000

    Initial comit

---
+++ Tutorial1/Assets/scripts/PlayerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    const int MOVEMENT_RATE = 30;
+    const int ROTATION_RATE = 150;
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * ROTATION_RATE;
+        var verticalInput = Input.GetAxis("Vertical") * Time.deltaTime * MOVEMENT_RATE;
+
+        transform.Rotate(0, horizontalInput, 0);
+        transform.Translate(0, 0, verticalInput);
+    }
+}
